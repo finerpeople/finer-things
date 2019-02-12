ac5d50980ae3e3dc34e10b6413cd355ef4e978c1
Leesha Miller <leesha.miller11@gmail.com>
2019-02-12 15:30:29 -0700

    browse

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Book from './../Book/Book';
+import {Link} from 'react-router-dom';
 import "./Browse.scss";
 
 const key = process.env.REACT_APP_NYT
 
 export default class Browse extends Component {
+  state = {
+    fictionList: []
+  }
   componentDidMount() {
     axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:9780735219113')
-    .then(res => {
-      console.log(res.data)
-    })
+      .then(res => {
+        console.log(res.data)
+      })
     axios
       .get(
-        `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${key}`
+        // `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${key}`
+        `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=eqLOyVkh9IvUeRF7ktALUnsxSMRJ9s52`
       )
       .then(res => {
-        console.log(res.data.results.lists);
+        this.setState({
+          fictionList: res.data.results.books
+        })
+        console.log(res.data.results.books);
+
       });
-      // let books = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=40&fields=items(id, volumeInfo/title, volumeInfo/authors, volumeInfo/description, volumeInfo/industryIdentifiers, volumeInfo/categories, volumeInfo/averageRating, volumeInfo/imageLinks, volumeInfo/previewLink)`)
+    // let books = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=40&fields=items(id, volumeInfo/title, volumeInfo/authors, volumeInfo/description, volumeInfo/industryIdentifiers, volumeInfo/categories, volumeInfo/averageRating, volumeInfo/imageLinks, volumeInfo/previewLink)`)
   }
 
   render() {
+    let fictionDisplay = this.state.fictionList.map((book, i) => {
+      // <Book 
+      // key={i}
+      // isbn={book.primary_isbn13}
+      // />
+      return (
+        <Link to={`book/${book.primary_isbn13}` }>
+          <img src={book.book_image} alt='book cover'  />
+        </Link>
+      )
+    })
     return (
       <div>
-        Here is the Browser
+        <h1>Here is the Browser</h1>
+        {fictionDisplay}
       </div>
     );
   }
