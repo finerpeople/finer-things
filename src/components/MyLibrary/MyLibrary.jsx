5514abad6ed7dbe8d5e774e84446b95019ac5f5d
Michelle Gargalis <michelle7997@yahoo.com>
2019-02-12 16:48:50 -0700

    add z-index and border to nav

--- src/components/MyLibrary/MyLibrary.jsx
+++ src/components/MyLibrary/MyLibrary.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Book from '../Book/Book'
+// import Book from '../Book/Book'
 import './MyLibrary.scss'
 
 export default class MyLibrary extends Component {
   render() {
     return (
       <div>
-        <Book />
+        {/* <Book /> */}
       </div>
     )
   }
