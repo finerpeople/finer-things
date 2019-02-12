f282c42e6df2ac59536a118460239c2ca88911e4
distoneman <distoneman@gmail.com>
2019-02-12 11:00:52 -0700

    initial commit

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -3,7 +3,24 @@ import axios from 'axios'
 import './Browse.scss'
 
 export default class Browse extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        books: [],
+    }
+  }
+
+  async componentDidMount() {
+    let books = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=40&fields=items(id, volumeInfo/title, volumeInfo/authors, volumeInfo/description, volumeInfo/industryIdentifiers, volumeInfo/categories, volumeInfo/averageRating, volumeInfo/imageLinks, volumeInfo/previewLink)
+    `).then(res => {
+      this.setState({ books: res.data });
+      console.log(res.data.items)
+      console.log(this.state.books)
+  })
+}
+  
   render() {
+    
     return (
       <div>
         
