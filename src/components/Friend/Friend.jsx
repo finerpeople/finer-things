5f2c8d86a03b1636553c2bf8add297f4b15ab2de
James Page <jcpage02@outlook.com>
2019-02-14 17:04:40 -0700

    Friend search input and button sytling done

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import './Friend.scss'
+import React, { Component } from "react";
+import axios from "axios";
+import "./Friend.scss";
 
 export default class Friend extends Component {
   componentDidMount = async () => {
-    this.getSession()
+    this.getSession();
   };
 
   getSession = async () => {
-    const res = await axios.get("/api/session")
-      if (!res.data.loggedIn) {
-        this.props.history.push("/");
-      }
-  }
+    const res = await axios.get("/api/session");
+    if (!res.data.loggedIn) {
+      this.props.history.push("/");
+    }
+  };
 
   render() {
     return (
-      <div >
-        
+      <div id="friend">
+        <div id="friend-header">
+          <input className="friend-header-input" type="text" placeholder='Search'/>
+          <div className='friend-header-btn'>
+            <i className="fas fa-search" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
